7183e63d27dab094e051c10f2ad67352872c5bd8
Andrew Ngo <andrewtruongngo@gmail.com>
2019-05-05 18:51:39 -0700

    Setup typescript and react

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import { Hello } from './components/Hello';
+
+// ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('app'));
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
